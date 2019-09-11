13aa71268091244b2f1195c1fddea664c226962c
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-10 21:32:11 -0400

    Created error message object

---
+++ src/UptimeHippoApi.Common/Exception/ErrorMessage.cs
@@ -0,0 +1,17 @@
+﻿namespace UptimeHippoApi.Common.Exception
+{
+    public class ErrorMessage
+    {
+        public ErrorMessage(string message)
+        {
+            Message = message;
+        }
+
+        public ErrorMessage(System.Exception exception)
+        {
+            Message = exception.Message;
+        }
+
+        public string Message { get; set; }
+    }
+}
