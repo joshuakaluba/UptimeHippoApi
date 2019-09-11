8bc777feca01fe8efde54430667a490325211e1f
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-10 21:17:12 -0400

    Created token object

---
+++ src/UptimeHippoApi.Data/Models/Authentication/Token.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace UptimeHippoApi.Data.Models.Authentication
+{
+    public class Token
+    {
+        [JsonProperty("accessToken")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("expiryDate")]
+        public System.DateTime ExpiryDate { get; set; }
+    }
+}
