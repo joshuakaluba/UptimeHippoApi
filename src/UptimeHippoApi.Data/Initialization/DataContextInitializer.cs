a7dc027df01742001df085e38b43e095e6a6cd0f
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-10 21:01:35 -0400

    Created data context initializer

---
+++ src/UptimeHippoApi.Data/Initialization/DataContextInitializer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UptimeHippoApi.Data.DataContext;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace UptimeHippoApi.Data.Initialization
+{
+    public static class DataContextInitializer
+    {
+        public static Claim DefaultUserClaim => new Claim("DefaultUserClaim", "DefaultUserAuthorization");
+        public static string AdministratorRole => "Administrator";
+        public static string UserRole => "User";
+        private static readonly string[] Roles = new string[] { AdministratorRole, UserRole };
+
+        public static async Task Seed(IServiceProvider serviceProvider)
+        {
+            using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<UptimeHippoDataContext>();
+
+                if (context.Database.GetPendingMigrations().Any())
+                {
+                    await context.Database.MigrateAsync();
+                    var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    foreach (var role in Roles)
+                    {
+                        if (!await roleManager.RoleExistsAsync(role))
+                        {
+                            await roleManager.CreateAsync(new IdentityRole(role));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
