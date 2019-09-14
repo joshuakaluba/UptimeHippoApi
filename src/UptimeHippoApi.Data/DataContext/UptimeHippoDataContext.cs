2a0804734bed220c1b2f952546b6d94566bf6196
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-14 10:12:02 -0400

    Created application data context c

---
+++ src/UptimeHippoApi.Data/DataContext/UptimeHippoDataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UptimeHippoApi.Data.Models.Authentication;
+using UptimeHippoApi.Data.Models.Notification;
+using UptimeHippoApi.Data.Models.Static;
+
+namespace UptimeHippoApi.Data.DataContext
+{
+    public class UptimeHippoDataContext : IdentityDbContext<ApplicationUser>
+    {
+        internal DbSet<PushNotificationToken> PushNotificationTokens { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString
+                = $"Server={ApplicationConfig.DatabaseHost};" +
+                    $"database={ApplicationConfig.DatabaseName};" +
+                        $"uid={ApplicationConfig.DatabaseUser};" +
+                            $"pwd={ApplicationConfig.DatabasePassword};" +
+                                "pooling=true;";
+
+            optionsBuilder.UseMySql(connectionString);
+        }
+    }
+}
