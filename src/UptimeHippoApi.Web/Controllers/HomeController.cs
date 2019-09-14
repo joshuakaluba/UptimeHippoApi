5c96495d9a424be2916a609e08904f2783e7ecfc
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-14 09:44:35 -0400

    Created HomeController

---
+++ src/UptimeHippoApi.Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using UptimeHippoApi.Data.Models.Authentication;
+using UptimeHippoApi.Data.Models.Domain.ViewModels;
+
+namespace UptimeHippoApi.Web.Controllers
+{
+    public class HomeController : BaseController<HomeController>
+    {
+        public HomeController(UserManager<ApplicationUser> userManager,
+            ILogger<HomeController> logger)
+            : base(userManager, logger)
+        {
+        }
+
+        //Home/Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                Logger.LogError(ex.Message);
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        //Home/Health
+        [HttpGet]
+        public IActionResult Health()
+        {
+            return Ok();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
