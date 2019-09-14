d50c34ed18034154c1e10314afe58636342da0e3
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-14 09:57:19 -0400

    Created application config

---
+++ src/UptimeHippoApi.Data/Models/Static/ApplicationConfig.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace UptimeHippoApi.Data.Models.Static
+{
+    public static class ApplicationConfig
+    {
+        #region Database Configs
+
+        internal static string DatabaseHost
+           = Environment.GetEnvironmentVariable
+               ("UPTIME_HIPPO_API_DB_HOST", target: EnvironmentVariableTarget.Process);
+
+        public static string Port
+            = Environment.GetEnvironmentVariable
+                ("UPTIME_HIPPO_API_APPLICATION_PORT", target: EnvironmentVariableTarget.Process);
+
+        internal static string DatabaseName
+            = Environment.GetEnvironmentVariable
+                ("UPTIME_HIPPO_API_DB_NAME", target: EnvironmentVariableTarget.Process);
+
+        internal static string DatabaseUser
+            = Environment.GetEnvironmentVariable
+                ("UPTIME_HIPPO_API_DB_USER", target: EnvironmentVariableTarget.Process);
+
+        internal static string DatabasePassword
+            = Environment.GetEnvironmentVariable
+                ("UPTIME_HIPPO_API_DB_PASSWORD", target: EnvironmentVariableTarget.Process);
+
+        #endregion Database Configs
+
+        #region Misc
+
+        public static string JwtTokenKey
+           = Environment.GetEnvironmentVariable
+               ("UPTIME_HIPPO_API_JWT_KEY", target: EnvironmentVariableTarget.Process);
+
+        #endregion Misc
+
+        #region Twilio Config
+
+        internal static bool SendTwillioMessage => Environment.GetEnvironmentVariable
+                ("UPTIME_HIPPO_API_SEND_TWILIO_MESSAGE", target: EnvironmentVariableTarget.Process).ToLower().Contains("true");
+
+        public static string TwilioAccountSId
+            = Environment.GetEnvironmentVariable
+                ("TWILIO_ACCOUNT_SID", target: EnvironmentVariableTarget.Process);
+
+        public static string TwilioAuthenticationToken
+            = Environment.GetEnvironmentVariable
+                ("TWILIO_AUTHENTICATION_TOKEN", target: EnvironmentVariableTarget.Process);
+
+        public static string TwilioSenderPhoneNumber
+            = Environment.GetEnvironmentVariable
+                ("TWILIO_SENDER_PHONENUMBER", target: EnvironmentVariableTarget.Process);
+
+        public static string TwilioRecipientPhoneNumber
+            = Environment.GetEnvironmentVariable
+                ("TWILIO_RECIPIENT_PHONENUMBER", target: EnvironmentVariableTarget.Process);
+
+        #endregion Twilio Config
+    }
+}
