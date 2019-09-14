ef053b5995ab7f23b2fbd748eeb4185a65759b50
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-14 09:41:40 -0400

    Created IUserRepository and implementation

---
+++ src/UptimeHippoApi.Data/DataAccessLayer/Authentication/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using UptimeHippoApi.Data.Models.Authentication;
+
+namespace UptimeHippoApi.Data.DataAccessLayer.Authentication
+{
+    public interface IUserRepository
+    {
+        Task<IdentityResult> RegisterNewUser(UserManager<ApplicationUser> userManager, ApplicationUser user, string password);
+    }
+}
---
+++ src/UptimeHippoApi.Data/DataAccessLayer/Authentication/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using UptimeHippoApi.Data.Initialization;
+using UptimeHippoApi.Data.Models.Authentication;
+
+namespace UptimeHippoApi.Data.DataAccessLayer.Authentication
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        public async Task<IdentityResult> RegisterNewUser(UserManager<ApplicationUser> userManager, ApplicationUser user, string password)
+        {
+            var result = await userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+                await userManager.AddClaimAsync(user, DataContextInitializer.DefaultUserClaim);
+                await userManager.AddToRoleAsync(user, "User");
+            }
+
+            return result;
+        }
+    }
+}
