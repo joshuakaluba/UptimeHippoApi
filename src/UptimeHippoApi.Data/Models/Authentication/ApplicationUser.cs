8c66367aac96a1d3f7e86504f9b20de027dc81ee
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-14 09:40:39 -0400

    Created application user class

---
+++ src/UptimeHippoApi.Data/Models/Authentication/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace UptimeHippoApi.Data.Models.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public bool Active { get; set; } = true;
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    }
+}
