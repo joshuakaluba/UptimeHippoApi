6e5259ced58882606fdf979a95d6c6adba819311
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-14 09:44:01 -0400

    Created BaseController

---
+++ src/UptimeHippoApi.Services/Authentication/UserValidator.cs
@@ -0,0 +1,12 @@
+﻿using UptimeHippoApi.Data.Models.Authentication;
+
+namespace UptimeHippoApi.Services.Authentication
+{
+    public static class UserValidator
+    {
+        public static bool Validate(ApplicationUser user)
+        {
+            return user.Active;
+        }
+    }
+}
---
+++ src/UptimeHippoApi.Web/Controllers/BaseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using UptimeHippoApi.Data.Models.Authentication;
+
+namespace UptimeHippoApi.Web.Controllers
+{
+    public abstract class BaseController<T> : Controller
+    {
+        protected readonly UserManager<ApplicationUser> UserManager;
+        protected ILogger<T> Logger;
+
+        protected BaseController(UserManager<ApplicationUser> userManager, ILogger<T> logger)
+        {
+            UserManager = userManager;
+            Logger = logger;
+        }
+
+        protected string GetUserId()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return userId;
+        }
+
+        protected async Task<ApplicationUser> GetUser()
+        {
+            var user = await UserManager.FindByIdAsync(GetUserId());
+            return user;
+        }
+    }
+}
