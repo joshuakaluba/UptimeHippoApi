2cd4dd9c4b99f6d8d9d57c242f8c85e14596ad64
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-15 18:48:06 -0400

    Created monitor interval enum

---
+++ src/UptimeHippoApi.Data/Models/Domain/Entity/MonitorIntervalEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace UptimeHippoApi.Data.Models.Domain.Entity
+{
+    public enum MonitorIntervalEnum
+    {
+        FIVE_MINUTES = 0,
+        TEN_MINUTES = 1,
+        THIRTY_MINUTES = 2,
+        HOURLY = 3,
+        TWELVE_HOURS = 4,
+        DAILY = 5
+    }
+}
