ab63ccfb882d473574f0d77705970bd68a1f5ea7
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-28 14:22:55 -0400

    Create AccountViewModels.cs

---
+++ src/UptimeHippoApi.Data/Models/Authentication/AccountViewModels.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace UptimeHippoApi.Data.Models.Authentication
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+    public class PushNotificationModel
+    {
+        [Required]
+        public string Token { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [JsonProperty("confirmPassword")]
+        public string ConfirmPassword { get; set; }
+    }
+}
