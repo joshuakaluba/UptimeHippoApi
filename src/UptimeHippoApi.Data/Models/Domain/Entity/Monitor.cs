552cd4f813448df505024574e731ab4dc996c6c9
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-28 14:21:57 -0400

    Create Monitor.cs

---
+++ src/UptimeHippoApi.Data/Models/Domain/Entity/Monitor.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using UptimeHippoApi.Data.Models.Authentication;
+using UptimeHippoApi.Data.Models.Domain.Entity;
+
+namespace UptimeHippoApi.Data.Models.WebResource
+{
+    public class Monitor : Auditable
+    {
+        public Monitor(string url)
+        {
+            Url = url;
+        }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("type")]
+        public MonitorTypeEnum Type { get; set; }
+
+        [JsonProperty("interval")]
+        public MonitorIntervalEnum Interval { get; set; }
+
+        [JsonProperty("active")]
+        public bool Active { get; set; } = true;
+
+        public string ApplicationUserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
