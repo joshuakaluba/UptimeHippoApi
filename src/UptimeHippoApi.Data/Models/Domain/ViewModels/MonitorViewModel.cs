0941801e689758da34197be5f8a1f0b2653a3b2e
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-28 14:22:04 -0400

    Create MonitorViewModel.cs

---
+++ src/UptimeHippoApi.Data/Models/Domain/ViewModels/MonitorViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using UptimeHippoApi.Data.Models.Domain.Entity;
+using UptimeHippoApi.Data.Models.WebResource;
+
+namespace UptimeHippoApi.Data.Models.Domain.ViewModels
+{
+    public class MonitorViewModel
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("type")]
+        public MonitorTypeEnum Type { get; set; }
+
+        [JsonProperty("interval")]
+        public MonitorIntervalEnum Interval { get; set; }
+
+        [JsonProperty("active")]
+        public bool Active { get; set; } = true;
+    }
+}
