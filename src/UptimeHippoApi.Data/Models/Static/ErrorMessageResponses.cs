1d26d10f8b26fc1f182a2fb3d053489d815bd420
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-10 21:44:57 -0400

    Created error message response object

---
+++ src/UptimeHippoApi.Data/Models/Static/ErrorMessageResponses.cs
@@ -0,0 +1,10 @@
+﻿namespace UptimeHippoApi.Data.Models.Static
+{
+    public static class ErrorMessageResponses
+    {
+        public static string UnableToLogIn => "Unable to log in";
+        public static string UnableToRegister => "Unable to register";
+        public static string IncompleteDataReceived => "Incomplete data received";
+        public static string AccountDeactivated => "Account deactivated";
+    }
+}
