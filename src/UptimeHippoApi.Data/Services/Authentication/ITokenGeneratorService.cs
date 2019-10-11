bbc3c39946c1690873ad0236708f2229c390e315
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-11 17:47:48 -0400

    Create ITokenGeneratorService.cs

---
+++ src/UptimeHippoApi.Data/Services/Authentication/ITokenGeneratorService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using UptimeHippoApi.Data.Models.Authentication;
+using TokenOptions = UptimeHippoApi.Data.Models.Authentication.TokenOptions;
+
+namespace UptimeHippoApi.Data.Services.Authentication
+{
+    public interface ITokenGeneratorService
+    {
+        Task<Token> CreateJwtToken(ApplicationUser user, UserManager<ApplicationUser> userManager, TokenOptions tokenOptions);
+    }
+}
