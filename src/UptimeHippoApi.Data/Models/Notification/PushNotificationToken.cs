737803a9c9fefb605210835c10859dcac1ae8a5f
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-10 21:44:28 -0400

    Create PushNotificationToken.cs

---
+++ src/UptimeHippoApi.Data/Models/Notification/PushNotificationToken.cs
@@ -0,0 +1,19 @@
+﻿using UptimeHippoApi.Data.Models.Authentication;
+using UptimeHippoApi.Data.Models.Domain.Entity;
+
+namespace UptimeHippoApi.Data.Models.Notification
+{
+    public class PushNotificationToken : Auditable
+    {
+        public string Token { get; set; }
+        public bool Active { get; set; } = true;
+        public string ApplicationUserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        public override bool Equals(Auditable other)
+        {
+            var token = (PushNotificationToken)other;
+            return Token == token.Token;
+        }
+    }
+}
