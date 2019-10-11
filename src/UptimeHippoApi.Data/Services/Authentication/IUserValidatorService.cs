9df0e989b0f0bba75d30cf5e83499ed865d2191d
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-11 18:08:07 -0400

    Created interface for user validation

---
+++ src/UptimeHippoApi.Data/Services/Authentication/IUserValidatorService.cs
@@ -0,0 +1,9 @@
+﻿using UptimeHippoApi.Data.Models.Authentication;
+
+namespace UptimeHippoApi.Data.Services.Authentication
+{
+    public interface IUserValidatorService
+    {
+        bool ValidateUser(ApplicationUser user);
+    }
+}
