54128af713b7f31983439d547fc70c7b8e3f86a1
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-10 21:47:48 -0400

    Created MonitorLog class

---
+++ src/UptimeHippoApi.Data/Models/Domain/Entity/MonitorLog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UptimeHippoApi.Data.Models.Domain.Entity
+{
+    public class MonitorLog : Auditable
+    {
+    }
+}
