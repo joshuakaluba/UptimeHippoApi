3538063dcbf6aa80545b7405286b06a02d29e01e
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-10 21:44:10 -0400

    Created interface and implementation for push notifications repository

---
+++ src/UptimeHippoApi.Data/DataAccessLayer/PushNotificationTokens/IPushNotificationTokensRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using UptimeHippoApi.Data.Models.Notification;
+
+namespace UptimeHippoApi.Data.DataAccessLayer.PushNotificationTokens
+{
+    public interface IPushNotificationTokensRepository
+    {
+        Task SavePushNotificationToken(PushNotificationToken token);
+
+        Task RemovePushNotificationToken(PushNotificationToken token);
+    }
+}
---
+++ src/UptimeHippoApi.Data/DataAccessLayer/PushNotificationTokens/PushNotificationTokensRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using UptimeHippoApi.Data.DataContext;
+using UptimeHippoApi.Data.Models.Notification;
+
+namespace UptimeHippoApi.Data.DataAccessLayer.PushNotificationTokens
+{
+    public class PushNotificationTokensRepository : BaseRepository, IPushNotificationTokensRepository
+    {
+        public async Task SavePushNotificationToken(PushNotificationToken token)
+        {
+            using (DataContext = new UptimeHippoDataContext())
+            {
+                DataContext.PushNotificationTokens
+                    .RemoveRange(DataContext.PushNotificationTokens
+                        .Where(t => t.Token == token.Token));
+
+                DataContext.PushNotificationTokens.Add(token);
+
+                await DataContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task RemovePushNotificationToken(PushNotificationToken token)
+        {
+            using (DataContext = new UptimeHippoDataContext())
+            {
+                DataContext.PushNotificationTokens
+                    .RemoveRange(DataContext.PushNotificationTokens
+                        .Where(t => t.Token == token.Token));
+
+                await DataContext.SaveChangesAsync();
+            }
+        }
+    }
+}
