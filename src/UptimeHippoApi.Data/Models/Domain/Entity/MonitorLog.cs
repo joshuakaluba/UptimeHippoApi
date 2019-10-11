e962b0af5e2008cbda59e085744d24676bf3c269
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-10 22:32:05 -0400

    Added success status and exception message property

--- src/UptimeHippoApi.Data/Models/Domain/Entity/MonitorLog.cs
+++ src/UptimeHippoApi.Data/Models/Domain/Entity/MonitorLog.cs
@@ -5,9 +5,9 @@ namespace UptimeHippoApi.Data.Models.Domain.Entity
     public class MonitorLog : Auditable
     {
         public int ResponseCode { get; set; }
-
+        public string ExceptionMessage { get; set; }
+        public bool Successful { get; set; }
         public Guid MonitorId { get; set; }
-
         public virtual Monitor Monitor { get; set; }
     }
 }
