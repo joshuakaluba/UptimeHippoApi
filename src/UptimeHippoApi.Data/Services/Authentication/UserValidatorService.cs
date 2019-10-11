8e15df7c9ba90a135d51b023062bd8434da30bae
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-11 18:08:25 -0400

    Created temporary implementaion for user validation

---
+++ src/UptimeHippoApi.Data/Services/Authentication/UserValidatorService.cs
@@ -0,0 +1,14 @@
+﻿using UptimeHippoApi.Data.Models.Authentication;
+
+namespace UptimeHippoApi.Data.Services.Authentication
+{
+    public class UserValidatorService : IUserValidatorService
+    {
+        //TODO implement true user validation
+        //that is more than checking if account is active
+        public bool ValidateUser(ApplicationUser user)
+        {            
+            return user.Active;
+        }
+    }
+}
