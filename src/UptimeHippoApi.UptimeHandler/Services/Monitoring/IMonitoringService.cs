ed7853f9b9493c843569a2bbeb9bf48a521389f5
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-13 10:07:42 -0400

    Added monitor logs repository

--- src/UptimeHippoApi.UptimeHandler/Services/Monitoring/IMonitoringService.cs
+++ src/UptimeHippoApi.UptimeHandler/Services/Monitoring/IMonitoringService.cs
@@ -1,10 +1,11 @@
 ﻿using System.Threading.Tasks;
+using UptimeHippoApi.Data.DataAccessLayer.MonitorLogs;
 using UptimeHippoApi.Data.DataAccessLayer.Monitors;
 
 namespace UptimeHippoApi.UptimeHandler.Services.Monitoring
 {
     internal interface IMonitoringService
     {
-        Task Monitor(IMonitorsRepository monitorsRepository);
+        Task Monitor(IMonitorsRepository monitorsRepository, IMonitorLogsRepository monitorLogRepository);
     }
 }
