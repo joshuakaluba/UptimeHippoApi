a5034acad0682c009c9c7d67cb287291c0f1e6b4
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-13 10:06:28 -0400

    Created interface for monitoring service

---
+++ src/UptimeHippoApi.UptimeHandler/Services/Monitoring/IMonitoringService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using UptimeHippoApi.Data.DataAccessLayer.Monitors;
+
+namespace UptimeHippoApi.UptimeHandler.Services.Monitoring
+{
+    internal interface IMonitoringService
+    {
+        Task Monitor(IMonitorsRepository monitorsRepository);
+    }
+}
