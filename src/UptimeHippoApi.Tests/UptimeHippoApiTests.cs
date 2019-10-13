175bdca308ab7b6036f95729b3c3f110e4900704
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-13 18:17:48 -0400

    Created initial test cases

--- src/UptimeHippoApi.Tests/UptimeHippoApiTests.cs
+++ src/UptimeHippoApi.Tests/UptimeHippoApiTests.cs
@@ -1,23 +1,85 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.NetworkInformation;
+using UptimeHippoApi.Common.Exception;
 using UptimeHippoApi.Common.Messaging;
+using UptimeHippoApi.Common.Utilities;
 using UptimeHippoApi.Data.Models.Static;
 
 namespace UptimeHippoApi.Tests
 {
-    //TODO write tests
     [TestClass]
     public class UptimeHippoApiTests
     {
         [TestMethod]
-        [ExpectedException(typeof(System.AggregateException))]
-        public void TestMethod()
+        [ExpectedException(typeof(KeyWordNotFoundException))]
+        public void TestGetKeywordFailing()
         {
-            throw new System.AggregateException();
+            var html = @" <!doctype html>
+                        <html lang='en'>
+                          <head>
+                            <!-- Required meta tags -->
+                            <meta charset='utf-8'>
+                            <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+                            <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'>
+                            <title>Hello World</title>
+                          </head>
+                          <body>
+                            <div class='container'>
+                                <p> Hello World </p>
+                            </div>
+                            <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>
+                            <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'></script>
+                            <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'></script>
+                          </body>
+                        </html>";
+
+            var keyWord = "Microsoft";
+
+            MonitoringHelper.EnsureKeyWordExists(html, keyWord);
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void TestGetKeyword()
         {
+            var html = @" <!doctype html>
+                        <html lang='en'>
+                          <head>
+                            <!-- Required meta tags -->
+                            <meta charset='utf-8'>
+                            <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+                            <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'>
+                            <title>Hello World</title>
+                          </head>
+                          <body>
+                            <div class='container'>
+                                <p> Hello World </p>
+                            </div>
+                            <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>
+                            <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'></script>
+                            <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'></script>
+                          </body>
+                        </html>";
+
+            var keyWord = "Hello World";
+
+            MonitoringHelper.EnsureKeyWordExists(html, keyWord);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(PingException))]
+        public void TestFailingSitePing()
+        {
+            var fakeUrl = "joshuakaluba.kaluba";
+
+            MonitoringHelper.PingHost(fakeUrl);
+        }
+
+        [TestMethod]
+        public void TestPing()
+        {
+            var urlToTest = "www.google.com";
+
+            MonitoringHelper.PingHost(urlToTest);
         }
 
         [TestMethod]
