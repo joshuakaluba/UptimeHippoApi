41cfd2b75674d2635490f8fab2d1f57084e647c8
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-14 20:44:40 -0400

    Added updated list of monitors and get all moniters methods

--- src/UptimeHippoApi.Data/DataAccessLayer/Monitors/IMonitorsRepository.cs
+++ src/UptimeHippoApi.Data/DataAccessLayer/Monitors/IMonitorsRepository.cs
@@ -15,7 +15,11 @@ namespace UptimeHippoApi.Data.DataAccessLayer.Monitors
         Task<Monitor> FindMonitor(Guid id);
 
         Task UpdateMonitor(Monitor monitor);
+        
+        Task UpdateMonitors(IEnumerable<Monitor> monitors);
 
         Task<IEnumerable<Monitor>> GetMonitorsByUser(IdentityUser user);
+
+        Task<IEnumerable<Monitor>> GetAllActiveMonitors();
     }
 }
