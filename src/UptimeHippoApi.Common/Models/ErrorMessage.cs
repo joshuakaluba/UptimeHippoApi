8dd377c2035ce2e8e9582998d50fd2e2a6ddf307
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-17 22:11:44 -0400

    Created common error message model

---
+++ src/UptimeHippoApi.Common/Models/ErrorMessage.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace UptimeHippoApi.Common.Models
+{
+    public class ErrorMessage
+    {
+        public ErrorMessage(string message)
+        {
+            Message = message;
+        }
+
+        public ErrorMessage(System.Exception exception)
+        {
+            Message = exception.Message;
+        }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
