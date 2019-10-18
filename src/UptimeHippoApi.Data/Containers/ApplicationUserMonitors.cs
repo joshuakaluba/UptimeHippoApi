59c322a0b374d0e798658ac7bef685b2922e5ddb
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-18 17:35:54 -0400

    Created initial application user monitor dictionary

---
+++ src/UptimeHippoApi.Data/Containers/ApplicationUserMonitors.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UptimeHippoApi.Data.Models.Authentication;
+using UptimeHippoApi.Data.Models.Domain.Entity;
+
+namespace UptimeHippoApi.Data.Containers
+{
+    public class ApplicationUserMonitors
+    {
+        public Dictionary<ApplicationUser, List<Monitor>> userMonitors;
+
+        public ApplicationUserMonitors()
+        {
+            userMonitors = new Dictionary<ApplicationUser, List<Monitor>>();
+        }
+
+        public void Add(Monitor monitor)
+        {
+            if (monitor.Triggered) { return; }
+
+            if (userMonitors.ContainsKey(monitor.ApplicationUser))
+            {
+                userMonitors[monitor.ApplicationUser].Add(monitor);
+            }
+            else
+            {
+                var monitorList = new List<Monitor>();
+                monitorList.Add(monitor);
+
+                userMonitors.Add(monitor.ApplicationUser, monitorList);
+            }
+        }
+    }
+}
