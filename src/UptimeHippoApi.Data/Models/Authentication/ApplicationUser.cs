75e951541d40a9790fb3ef5cd54da69279001142
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-17 22:09:16 -0400

    Added notifications enabled property

--- src/UptimeHippoApi.Data/Models/Authentication/ApplicationUser.cs
+++ src/UptimeHippoApi.Data/Models/Authentication/ApplicationUser.cs
@@ -6,6 +6,7 @@ namespace UptimeHippoApi.Data.Models.Authentication
     public class ApplicationUser : IdentityUser
     {
         public bool Active { get; set; } = true;
+        public bool NotificationsEnabled { get; set; } = true;
         public DateTime DateCreated { get; set; } = DateTime.UtcNow;
     }
 }
