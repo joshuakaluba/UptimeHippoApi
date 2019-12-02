ecbe28e5f69dfc8e9b00b9a29d2a678cdc84ea45
Joshua Kaluba <joshkaluba@gmail.com>
2019-12-01 19:12:33 -0500

    Create UserSettingsController.cs

---
+++ src/UptimeHippoApi.Web/Controllers/UserSettingsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using UptimeHippoApi.Common.Models;
+using UptimeHippoApi.Data.DataAccessLayer.Authentication;
+using UptimeHippoApi.Data.Models.Authentication;
+
+namespace UptimeHippoApi.Web.Controllers
+{
+    public sealed class UserSettingsController : BaseController<UserSettingsController>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserSettingsController(IUserRepository userRepository,
+            UserManager<ApplicationUser> userManager,
+            ILogger<UserSettingsController> logger)
+            : base(userManager, logger)
+        {
+            _userRepository = userRepository;
+        }
+
+        //UserSettings/GetMySettings
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "Trusted")]
+        public async Task<IActionResult> GetMySettings()
+        {
+            try
+            {
+                var user = await GetUser();
+                var userViewModel = new UserSettingsViewModel(user);
+
+                return Ok(userViewModel);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                return BadRequest(new ErrorMessage(ex));
+            }
+        }
+
+        //UserSettings/UpdateUserSettings
+        [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "Trusted")]
+        public async Task<IActionResult> UpdateUserSettings([FromBody] UserSettingsViewModel userSettingsViewModel)
+        {
+            try
+            {
+                var user = await GetUser();
+                user.UpdateFromUserViewModel(userSettingsViewModel);
+
+                await _userRepository.UpdateUser(UserManager, user);
+
+                return Ok(userSettingsViewModel);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                return BadRequest(new ErrorMessage(ex));
+            }
+        }
+    }
+}
