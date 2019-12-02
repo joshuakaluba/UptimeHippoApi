e314ac34a4a2faff537a101a8ba2b189a1e4ebad
Joshua Kaluba <joshkaluba@gmail.com>
2019-12-01 19:12:41 -0500

    Create PushMessage.cs

---
+++ src/UptimeHippoApi.UptimeHandler/Models/PushMessage.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace UptimeHippoApi.UptimeHandler.Models
+{
+    public class PushMessage
+    {
+        public PushMessage()
+        {
+        }
+
+        public PushMessage(string title, string body, string to)
+        {
+            Title = title;
+            Body = body;
+            To = to;
+        }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        [JsonProperty("to")]
+        public string To { get; set; }
+    }
+}
