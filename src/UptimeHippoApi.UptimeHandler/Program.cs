ea59a05000739ded7f8347d59e05e05a0763d81a
Joshua Kaluba <joshkaluba@gmail.com>
2019-12-01 19:14:13 -0500

    Added push notification repository, moved run static method to UptimeMonitor class

--- src/UptimeHippoApi.UptimeHandler/Program.cs
+++ src/UptimeHippoApi.UptimeHandler/Program.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
-using UptimeHippoApi.Data.Containers;
 using UptimeHippoApi.Data.DataAccessLayer.MonitorLogs;
 using UptimeHippoApi.Data.DataAccessLayer.Monitors;
+using UptimeHippoApi.Data.DataAccessLayer.PushNotificationTokens;
 using UptimeHippoApi.UptimeHandler.Services.Monitoring;
+using UptimeHippoApi.UptimeHandler.Services.PushNotification;
 
 namespace UptimeHippoApi.UptimeHandler
 {
@@ -15,34 +16,24 @@ namespace UptimeHippoApi.UptimeHandler
            .AddTransient<IMonitorsRepository, MonitorsRepository>()
            .AddTransient<IMonitorLogsRepository, MonitorLogsRepository>()
            .AddTransient<IMonitoringService, MonitoringService>()
+           .AddTransient<IPushNotificationService, PushNotificationService>()
+           .AddTransient<IPushNotificationTokensRepository, PushNotificationTokensRepository>()
            .BuildServiceProvider();
 
             var monitorsRepository = serviceProvider.GetService<IMonitorsRepository>();
             var monitoringService = serviceProvider.GetService<IMonitoringService>();
             var monitorLogsRepository = serviceProvider.GetService<IMonitorLogsRepository>();
+            var pushNotificationService = serviceProvider.GetService<IPushNotificationService>();
+            var pushNotificationTokensRepository = serviceProvider.GetService<IPushNotificationTokensRepository>();
 
             Task.Run(async () =>
             {
-                await Run(monitorsRepository, monitorLogsRepository, monitoringService);
+                await UptimeMonitor.Run(monitorsRepository,
+                    monitorLogsRepository,
+                    monitoringService,
+                    pushNotificationService,
+                    pushNotificationTokensRepository);
             }).GetAwaiter().GetResult();
-        }
-
-        private static async Task Run(IMonitorsRepository monitorsRepository,
-            IMonitorLogsRepository monitorLogsRepository,
-            IMonitoringService monitoringService)
-        {
-            await monitoringService.Monitor(null, monitorsRepository, monitorLogsRepository);
-
-            var failedMonitors = monitoringService.GetFailedMonitors();
-            var applicationUserMonitor = new ApplicationUserMonitors();
-
-            foreach (var monitor in failedMonitors)
-            {
-                if (!monitor.Triggered)
-                {
-                    applicationUserMonitor.Add(monitor);
-                }
-            }
         }
     }
 }
