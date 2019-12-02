320ea67fb2c51009728f3e5314ed16852f6df324
Joshua Kaluba <joshkaluba@gmail.com>
2019-12-01 19:12:07 -0500

    Create IMonitorLogsRepository.cs

---
+++ src/UptimeHippoApi.Data/DataAccessLayer/MonitorLogs/IMonitorLogsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UptimeHippoApi.Data.Models.Domain.Entity;
+
+namespace UptimeHippoApi.Data.DataAccessLayer.MonitorLogs
+{
+    public interface IMonitorLogsRepository
+    {
+        Task SaveMonitorLogs(IEnumerable<MonitorLog> monitorLogs);
+        Task<IEnumerable<MonitorLog>> GetMonitorLogsByMonitor(Monitor monitor);
+    }
+}
