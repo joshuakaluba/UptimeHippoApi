f011eb95381db08ebaa43d2437d4464d8a90beae
Joshua Kaluba <joshkaluba@gmail.com>
2019-12-08 14:36:09 -0500

    Created uptime monitor class

---
+++ src/UptimeHippoApi.UptimeHandler/UptimeMonitor.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using UptimeHippoApi.Data.DataAccessLayer.MonitorLogs;
+using UptimeHippoApi.Data.DataAccessLayer.Monitors;
+using UptimeHippoApi.Data.DataAccessLayer.PushNotificationTokens;
+using UptimeHippoApi.UptimeHandler.Services.Monitoring;
+using UptimeHippoApi.UptimeHandler.Services.PushNotification;
+
+namespace UptimeHippoApi.UptimeHandler
+{
+    public class UptimeMonitor
+    {
+        public static async Task Run(IMonitorsRepository monitorsRepository,
+            IMonitorLogsRepository monitorLogsRepository,
+            IMonitoringService monitoringService,
+            IPushNotificationService pushNotificationService,
+            IPushNotificationTokensRepository pushNotificationTokensRepository)
+        {
+            await monitoringService.Monitor(null, monitorsRepository, monitorLogsRepository);
+
+            var applicationUserMonitors = monitoringService.GetFailedMonitors();
+
+            await pushNotificationService.SendPushNotifications(applicationUserMonitors, pushNotificationTokensRepository);
+        }
+    }
+}
