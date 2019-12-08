8735ffb4088099dd756ef4ecc2e0d9462b259c8b
Joshua Kaluba <joshkaluba@gmail.com>
2019-12-08 14:34:49 -0500

    Created monitoring helper

---
+++ src/UptimeHippoApi.Common/Utilities/MonitoringHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.NetworkInformation;
+using UptimeHippoApi.Common.Exception;
+
+namespace UptimeHippoApi.Common.Utilities
+{
+    public class MonitoringHelper
+    {
+        public static bool EnsureKeyWordExists(string searchString, string keyword)
+        {
+            if (searchString.ToLower().Contains(keyword.ToLower()))
+            {
+                return true;
+            }
+            else
+            {
+                throw new KeyWordNotFoundException();
+            }
+        }
+
+        public static bool PingHost(string nameOrAddress)
+        {
+            using (var pinger = new Ping())
+            {
+                PingReply reply = pinger.Send(nameOrAddress);
+                return reply.Status == IPStatus.Success;
+            }
+        }
+    }
+}
