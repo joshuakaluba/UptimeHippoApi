199bb4dfc4c1efbeffc202f3b9f7e230f7f6bb46
Joshua Kaluba <joshkaluba@gmail.com>
2019-12-08 14:44:52 -0500

    Updated user properties, created update method

--- src/UptimeHippoApi.Data/Models/Authentication/ApplicationUser.cs
+++ src/UptimeHippoApi.Data/Models/Authentication/ApplicationUser.cs
@@ -6,7 +6,21 @@ namespace UptimeHippoApi.Data.Models.Authentication
     public class ApplicationUser : IdentityUser
     {
         public bool Active { get; set; } = true;
-        public bool NotificationsEnabled { get; set; } = true;
+
+        public bool PushNotificationsEnabled { get; set; } = true;
+
+        public bool NotificationTextMessagesEnabled { get; set; } = true;
+
+        public bool NotificationEmailsEnabled { get; set; } = true;
+
         public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        public void UpdateFromUserViewModel(UserSettingsViewModel userSettingsViewModel)
+        {
+            PhoneNumber = userSettingsViewModel.PhoneNumber;
+            NotificationTextMessagesEnabled = userSettingsViewModel.NotificationTextMessagesEnabled;
+            NotificationEmailsEnabled = userSettingsViewModel.NotificationEmailsEnabled;
+            PushNotificationsEnabled = userSettingsViewModel.PushNotificationsEnabled;
+        }
     }
 }
