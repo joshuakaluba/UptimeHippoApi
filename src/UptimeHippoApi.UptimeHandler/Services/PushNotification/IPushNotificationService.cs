07bb088239a750e95a7b4d85cecb2bbf8764c58d
Joshua Kaluba <joshkaluba@gmail.com>
2019-12-08 14:37:07 -0500

    Created push notification service

---
+++ src/UptimeHippoApi.UptimeHandler/Services/PushNotification/IPushNotificationService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using UptimeHippoApi.Data.Containers;
+using UptimeHippoApi.Data.DataAccessLayer.PushNotificationTokens;
+
+namespace UptimeHippoApi.UptimeHandler.Services.PushNotification
+{
+    public interface IPushNotificationService
+    {
+        Task SendPushNotifications(ApplicationUserMonitors applicationUserMonitors,
+            IPushNotificationTokensRepository pushNotificationTokensRepository);
+    }
+}
