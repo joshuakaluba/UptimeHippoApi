1be91a6792b68dd975dc753684fe8abd521b0bb2
Joshua Kaluba <joshkaluba@gmail.com>
2019-12-08 14:53:09 -0500

    Created user settings view model

---
+++ src/UptimeHippoApi.Data/Models/Authentication/UserSettingsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace UptimeHippoApi.Data.Models.Authentication
+{
+    public class UserSettingsViewModel
+    {
+        public UserSettingsViewModel()
+        {
+        }
+
+        public UserSettingsViewModel(ApplicationUser user)
+        {
+            Email = user.Email;
+            PhoneNumber = user.PhoneNumber;
+            NotificationTextMessagesEnabled = user.NotificationTextMessagesEnabled;
+            NotificationEmailsEnabled = user.NotificationEmailsEnabled;
+            PushNotificationsEnabled = user.PushNotificationsEnabled;
+        }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("phoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("notificationTextMessagesEnabled")]
+        public bool NotificationTextMessagesEnabled { get; set; }
+
+        [JsonProperty("notificationEmailsEnabled")]
+        public bool NotificationEmailsEnabled { get; set; }
+
+        [JsonProperty("pushNotificationsEnabled")]
+        public bool PushNotificationsEnabled { get; set; }
+    }
+}
